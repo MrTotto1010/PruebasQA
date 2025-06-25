43bed55be78058508c81eb83f29aafe814bfa1c8
MrTotto1010 <jose.totto1010@gmail.com>
2025-06-24 19:42:56 -0600

    FinalTest

---
+++ src/components/pages/storePage/ContainerStorePage.tsx
@@ -0,0 +1,16 @@
+import CardModel3d from "./CardModel3d";
+
+export default function ContainerStorePage(){
+    
+
+    return(
+
+    <div className="absolute h-full w-full bg-cardmon-gray" id="container">
+      
+    <CardModel3d></CardModel3d>
+
+
+    </div>
+    );
+
+}
